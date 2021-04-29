e1ea8f30f76e33e24023b84d5d9f4ec646d44c1a
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2021-04-29 10:11:40 +0200

    Add project files.

---
+++ Scala.DBProg.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scala.DBProg.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+        private void CmbFilterAuteur_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void CmbFilterUitgever_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnClearAuteurFilter_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnClearUitgeverFilter_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void LstBoeken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void LstAuteurs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnClearAuteur_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnAuteurToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnAuteurWijzigen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnAuteurVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void LstUitgevers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+        private void BtnClearUitgever_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnUitgeverToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnUitgeverWijzigen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void BtnUitgeverVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
